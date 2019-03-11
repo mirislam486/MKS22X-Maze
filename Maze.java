9aeb26434f835e9da77a09e2d17531e6aa466834
Mir Islam <mislam00@stuy.edu>
2019-03-11 01:27:47 -0400

    solve method

--- Maze.java
+++ Maze.java
@@ -26,13 +26,18 @@ public class Maze{
 
       }
 
+
+
       private void mazeArray(ArrayList<String> arr){
           maze = new char[arr.size()][(arr.get(0)).length()];
+
           for( int i = 0; i < arr.size(); i++){
             for( int z = 0; z < (arr.get(i)).length(); z++)
               maze[i][z] = (arr.get(i)).charAt(z);
           }
       }
+
+
 
       private void wait(int millis){
         try{
@@ -43,21 +48,50 @@ public class Maze{
         }
       }
 
+
+
       public void setAnimate(boolean b){
         animate = b;
       }
+
+
 
       public void clearTerminal(){
         System.out.println("\033[2J\033[1;1H");
       }
 
+
+
       public String toString(){
         return "";
       }
 
-      public int solve(){
-        return 0;
+
+
+      public int solve(int x, int y){
+        if(maze[x][y] == 'E'){
+          return 1;
+        }else{
+          if(placer(x, y)){
+            for(int i = 0; i < move.length; i++){
+              solve(x + move[i][0], y + move[i][0]);
+            }
+            move[x][y] = '.';
+          }
+          return -1;
+        }
       }
+
+      public boolean placer(int x, int y){
+        if(maze[x][y] == '@' || maze[x][y] == '#' || maze[x][y] == '.'){
+          return false;
+        }else{
+          maze[x][y] = '@';
+          return true;
+        }
+      }
+
+
 
       private int solve(int row, int col){
         if(animate){
